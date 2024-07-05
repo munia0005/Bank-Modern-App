891e1569173b8a00a5db6b702a5bd972d4a154d7
munia0005 <muniaaktar0005@gmail.com>
2024-07-05 18:36:09 +0600

    add src/components/

---
+++ src/assets/index.js
@@ -0,0 +1,54 @@
+import airbnb from "./airbnb.png";
+import binance from "./binance.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import facebook from "./facebook.png";
+import instagram from "./instagram.png";
+import linkedin from "./linkedin.png";
+import logo from "./logo.png";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import send from "./send.png";
+import shield from "./shield.png";
+import star from "./star.png";
+import twitter from "./twitter.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import Discount from "./Discount.svg";
+import robot from "./robot.png";
+import arrowUp from "./arrowUp.svg";
+import apple from "./apple.svg";
+import bill from "./bill.png";
+import google from "./google.svg";
+import card from "./card.png";
+import quotes from "./quotes.svg";
+
+
+export {
+    airbnb,
+    binance, 
+    coinbase,
+    facebook,
+    instagram,
+    linkedin,
+    logo,
+    people01,
+    people02,
+    people03,
+    dropbox,
+    send,
+    shield,
+    star,
+    twitter,
+    menu,
+    close,
+    Discount,
+    robot,
+    arrowUp,
+    apple,
+    bill,
+    google,
+    card,
+    quotes,
+}
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Stats from "./Stats";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+import Button from "./Button";
+import GetStarted from "./GetStarted";
+import FeedbackCard from "./FeedbackCard";
+
+export {
+    Navbar,
+    Billing,
+    Stats,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    Footer,
+    Hero,
+    Testimonials,
+    Button,
+    GetStarted,
+    FeedbackCard,
+};
